922aa05101e5e86e7260d8fc722e36e54c64e8fd
TaniyaHaghighi20 <81079805+TaniyaHaghighi20@users.noreply.github.com>
2021-11-06 20:25:53 +0330

    Add files via upload

---
+++ Project/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#mobile").text("09646003969").css("color","white");
+    $("#mobile").hide();
+   $("#whatsapp").click(function(){
+    $("#mobile").toggle(1000);
+   });
+
+  
+  });
