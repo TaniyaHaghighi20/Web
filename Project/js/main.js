4f64071c20d1bb7c07812bf348ff3bddb27d1025
TaniyaHaghighi20 <81079805+TaniyaHaghighi20@users.noreply.github.com>
2021-11-06 20:24:15 +0330

    Delete Project directory

--- Project/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#mobile").text("09646003969").css("color","white");
-    $("#mobile").hide();
-   $("#whatsapp").click(function(){
-    $("#mobile").toggle(1000);
-   });
-
-  
-  });
